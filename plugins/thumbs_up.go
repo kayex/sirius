30cb0ed86eb334c72c32a58e72f365f2c83e286c
Johan Vester <johan.vester@gmail.com>
2016-12-30 17:26:21 +0100

    Add ThumbsUp plugin

---
+++ plugins/thumbs_up.go
@@ -0,0 +1,14 @@
+package plugins
+
+import "github.com/kayex/sirius/model"
+
+const slackThumb string = ":+1:"
+
+type ThumbsUp struct{}
+
+func (tu *ThumbsUp) Run(m model.Message) []Transformation {
+	return []Transformation{
+		Substitute("(y)", slackThumb),
+		Substitute("(Y)", slackThumb),
+	}
+}
