b5aa5b32edfaffcdb549f6296f6abd323a9db13f
Johan Vester <johan.vester@gmail.com>
2016-12-30 17:26:33 +0100

    Add Ripperino plugin

---
+++ plugins/ripperino.go
@@ -0,0 +1,38 @@
+package plugins
+
+import (
+	"github.com/kayex/sirius/model"
+	"math/rand"
+	"strings"
+)
+
+var endings = []string{
+	"casino",
+	"linguini",
+	"bambino",
+	"ripperoni",
+}
+
+type Ripperino struct{}
+
+func (r *Ripperino) Run(m model.Message) []Transformation {
+	if !strings.HasPrefix(m.Text, "ripperino") {
+		return NoTransformation()
+	}
+
+	// 1 in 10 times, go full Grino
+	if rand.Int() % 10 == 1 {
+		return []Transformation{rapperGrino()}
+	}
+
+	return []Transformation{Append(getRandomEnding())}
+}
+
+func rapperGrino() Transformation {
+	return Substitute("ripperino", "~ripperino~ RAPPER GRINO")
+}
+
+func getRandomEnding() string {
+	e := rand.Int() % len(endings)
+	return endings[e]
+}
