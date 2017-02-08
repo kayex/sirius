69b20d37d8d91e5bed7d56457618363e35faf523
Johan Vester <johan.vester@gmail.com>
2017-02-08 21:35:24 +0100

    Use normal strings in config literal

--- config/env_config.go
+++ config/env_config.go
@@ -5,11 +5,11 @@ import "os"
 func FromEnv() AppConfig {
 	return AppConfig{
 		DB: DBConfig{
-			Host:     env(`DB_HOST`, `127.0.0.1`),
-			Port:     env(`DB_PORT`, `5432`),
-			Database: env(`DB_DATABASE`, ``),
-			User:     env(`DB_USER`, ``),
-			Password: env(`DB_PASSWORD`, ``),
+			Host:     env("DB_HOST", "127.0.0.1"),
+			Port:     env("DB_PORT", "5432"),
+			Database: env("DB_DATABASE", ""),
+			User:     env("DB_USER", ""),
+			Password: env("DB_PASSWORD", ""),
 		},
 		Maps: MapsConfig{
 			APIKey: env("MAPS_API_KEY", ""),
