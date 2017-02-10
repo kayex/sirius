97b572dd4854be8496e9bc7be9ffd5e62519be45
Johan Vester <johan.vester@gmail.com>
2017-02-10 18:07:14 +0100

    #24 Add support for adding and removing clients while service is running

--- sirius.go
+++ sirius.go
@@ -6,26 +6,69 @@ import (
 
 type Service struct {
 	loader  ExtensionLoader
-	clients []Client
+	clients map[string]*CancelClient
+	ctx     context.Context
+}
+
+type CancelClient struct {
+	Client
+	Cancel context.CancelFunc
+}
+
+func (c *Client) WithCancel(cancel context.CancelFunc) *CancelClient {
+	return &CancelClient{
+		Client: *c,
+		Cancel: cancel,
+	}
 }
 
 func NewService(l ExtensionLoader) *Service {
 	return &Service{
-		loader: l,
+		loader:  l,
+		clients: make(map[string]*CancelClient),
 	}
 }
 
 func (s *Service) Start(ctx context.Context, users []User) {
+	s.ctx = ctx
+
 	for _, u := range users {
 		u := u
-		cl := NewClient(&u, s.loader)
-		s.clients = append(s.clients, *cl)
-
-		go cl.Start(ctx)
+		s.startClient(&u)
 	}
 
-	select {
-	case <-ctx.Done():
-		break
+	for {
+		select {
+		case <-s.ctx.Done():
+			break
+		}
 	}
+}
+
+func (s *Service) AddUser(u *User) {
+	s.startClient(u)
+}
+
+func (s *Service) DropUserWithToken(t string) bool {
+	if cl, ok := s.clients[t]; ok {
+		cl.Cancel()
+
+		return true
+	}
+
+	return false
+}
+
+func (s *Service) startClient(u *User) {
+	// Make sure we stop any existing client for the same user
+	if ex, ok := s.clients[u.Token]; ok {
+		ex.Cancel()
+	}
+
+	ctx, cancel := context.WithCancel(s.ctx)
+	cl := NewClient(u, s.loader).WithCancel(cancel)
+
+	go cl.Start(ctx)
+
+	s.clients[u.Token] = cl
 }
