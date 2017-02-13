493dfbbdd24809cbed839bb5b8d83cfc4c076943
Johan Vester <johan.vester@gmail.com>
2017-02-13 01:04:17 +0100

    Add debug output

--- cmd/sirius-cloud/main.go
+++ cmd/sirius-cloud/main.go
@@ -18,14 +18,28 @@ func main() {
 		panic(err)
 	}
 
-	for _, u := range users {
-		fmt.Printf("[%v] %v cfgs\n", u.Token, len(u.Configurations))
-	}
-
 	ld := extension.NewStaticLoader(cfg)
 	sync := sirius.NewMQTTSync(rmt, cfg.MQTT.Config, cfg.MQTT.Topic)
 
 	s := sirius.NewService(ld).WithSync(sync)
 
+	printRunInfo(users, cfg)
+
 	s.Start(context.Background(), users)
+}
+
+func printRunInfo(users []sirius.User, cfg config.AppConfig) {
+	fmt.Println("Connecting to remote: " + cfg.Remote.Host)
+	fmt.Printf("Establishing MQTT sync: %v@%v:%v [%v]\n", cfg.MQTT.CID, cfg.MQTT.Host, cfg.MQTT.Port, cfg.MQTT.Topic)
+
+	for _, u := range users {
+		tks := ""
+		for i, c := range u.Configurations {
+			if i != 0 {
+				tks += ", "
+			}
+			tks += string(c.EID)
+		}
+		fmt.Printf("[%v] %v (%v)\n", u.ID.HashSum, len(u.Configurations), tks)
+	}
 }
