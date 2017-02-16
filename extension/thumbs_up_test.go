9731a1be6c0b4938ed364c7c7c77dfe114643746
Johan Vester <johan.vester@gmail.com>
2017-02-16 01:30:53 +0100

    Add thumbs_up_test

---
+++ extension/thumbs_up_test.go
@@ -0,0 +1,39 @@
+package extension
+
+import (
+	"testing"
+	"reflect"
+	"github.com/kayex/sirius"
+	"github.com/kayex/sirius/slack"
+)
+
+func TestThumbsUp_Run(t *testing.T) {
+	cases := []struct{
+		msg sirius.Message
+		exp sirius.MessageAction
+	}{
+		{
+			msg: sirius.NewMessage(slack.UserID{UserID: "123", TeamID: "abc"}, "(y)", "#channel", "0"),
+			exp: (&sirius.TextEditAction{}).Substitute("(y)", ":+1:").Substitute("(Y)", ":+1:"),
+		},
+		{
+			msg: sirius.NewMessage(slack.UserID{UserID: "123", TeamID: "abc"}, "Hej!", "#channel", "0"),
+			exp: (&sirius.TextEditAction{}).Substitute("(y)", ":+1:").Substitute("(Y)", ":+1:"),
+		},
+	}
+
+
+	for _, c := range cases {
+		tu := &ThumbsUp{}
+
+		act, err := tu.Run(c.msg, sirius.ExtensionConfig{})
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !reflect.DeepEqual(act, c.exp) {
+			t.Fatalf("Expected %s but got %s for message \"%s\"", c.exp, act, c.msg.Text)
+		}
+	}
+}
