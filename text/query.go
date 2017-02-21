c8936c0e0847a64c8df68bd5e0ac06c2cbc2deb9
Johan Vester <johan.vester@gmail.com>
2017-02-21 23:32:57 +0100

    Update Word query to treat all unicode space chars as delims

--- text/query.go
+++ text/query.go
@@ -1,13 +1,14 @@
 package text
 
-import "strings"
+import "unicode"
 
 type Query interface {
 	Match(string) bool
 }
 
-// Word matches only complete words, i.e. strings that
-// are not sub-strings of other words.
+// Word matches complete words only.
+// The "complete words" of a string s is defined as the result of
+// splitting the string on every single Unicode whitespace character.
 type Word struct {
 	W string
 }
@@ -16,20 +17,42 @@ func (q Word) Match(s string) bool {
 	if s == q.W {
 		return true
 	}
-	// "W lorem ipsum"
-	//  ^^
-	if strings.HasPrefix(s, q.W+" ") {
-		return true
+
+	sr := []rune(s)
+	qr := []rune(q.W)
+
+	if len(sr) < len(qr) {
+		return false
 	}
-	// "lorem ipsum W"
-	//             ^^
-	if strings.HasSuffix(s, " "+q.W) {
-		return true
-	}
-	// "lorem W ipsum"
-	//       ^^^
-	if strings.Contains(s, " "+q.W+" ") {
-		return true
+
+	var nMatch int
+
+	for i := 0; i < len(sr); i++ {
+		if nMatch < len(qr) {
+			if sr[i] != qr[nMatch] {
+				nMatch = 0
+				continue
+			}
+		}
+
+		nMatch++
+
+		if nMatch == len(qr) {
+			// Check that any immediately preceding or following
+			// characters are spaces.
+
+			next := i + 1
+			prev := i - nMatch
+			hasNext := len(sr) > next
+			hasPrev := i-nMatch >= 0
+
+			if hasNext && !unicode.IsSpace(sr[next]) || hasPrev && !unicode.IsSpace(sr[prev]) {
+				nMatch = 0
+				continue
+			}
+
+			return true
+		}
 	}
 
 	return false
