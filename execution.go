0cbd47541910c6c89879c745a535c0fa6dd78460
Johan Vester <johan.vester@gmail.com>
2017-03-06 15:21:26 +0100

    Use copied instead of captured Msg and Cfg variables

--- execution.go
+++ execution.go
@@ -40,7 +40,7 @@ func (r *AsyncRunner) Run(exe []Execution, res chan<- ExecutionResult, timeout t
 
 	for _, e := range exe {
 		go func(ex Execution, r chan<- ExecutionResult) {
-			a, err := ex.Ext.Run(e.Msg, e.Cfg)
+			a, err := ex.Ext.Run(ex.Msg, ex.Cfg)
 
 			r <- ExecutionResult{
 				Err:    err,
